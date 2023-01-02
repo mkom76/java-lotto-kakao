77131f800c9c5a62a4328dd295803a614ea8d470
person003333 <person00333@gmail.com>
2023-01-02 16:49:41 +0900

    feat : Lotto 당첨 순위 테스트

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -2,6 +2,7 @@ package lotto.model;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     List<Integer> lotto;
@@ -13,5 +14,36 @@ public class Lotto {
 
     public List<Integer> getLotto() {
         return lotto;
+    }
+
+    public int checkWin(List<Integer> winNumbers, int bonus) {
+        int matchedNum = lotto.stream()
+                .filter(winNumbers::contains)
+                .collect(Collectors.toSet())
+                .size();
+        return checkRank(matchedNum, bonus);
+    }
+
+    private int checkRank(int matchedNum, int bonus){
+        if(matchedNum == 3){
+            return 5;
+        }
+        if(matchedNum == 4){
+            return 4;
+        }
+        if(matchedNum == 5){
+            return checkBonus(bonus);
+        }
+        if(matchedNum == 6){
+            return 1;
+        }
+        return 6;
+    }
+
+    private int checkBonus(int bonus){
+        if(lotto.contains(bonus)){
+            return 2;
+        }
+        return 3;
     }
 }
--- src/test/java/lotto/model/LottoTest.java
+++ src/test/java/lotto/model/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class LottoTest {
     @Test
@@ -20,4 +21,23 @@ class LottoTest {
         Lotto lotto = new Lotto(Arrays.asList(3,4,5,6,1,2));
         Assertions.assertEquals(Arrays.asList(1,2,3,4,5,6), lotto.getLotto());
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,4,5,6;1",
+            "1,2,3,4,5,7;2",
+            "1,2,3,4,5,8;3",
+            "1,2,3,4,8,9;4",
+            "1,2,3,8,9,10;5",
+            "1,2,8,9,10,11;6"}, delimiter = ';')
+    void checkLottoWin(String input, String expect){
+        List<Integer> numbers = Arrays.stream(input.split(","))
+                                .map(Integer::parseInt)
+                                .collect(Collectors.toList());
+
+        List<Integer> winNumbers = Arrays.asList(1,2,3,4,5,6);
+        int bonus = 7;
+        Lotto lotto = new Lotto(numbers);
+        Assertions.assertEquals(lotto.checkWin(winNumbers, bonus), Integer.parseInt(expect));
+    }
+
 }
