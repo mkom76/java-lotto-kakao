c84c8989d54273fcbf8cc53f118758a340489e94
mkom76 <mkom76@yonsei.ac.kr>
2023-01-03 11:05:04 +0900

    [feat] 유저 인풋(돈) 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,25 @@
 package lotto.view;
 
 public class InputView {
+    public int receiveMoneyUserInput(String input) {
+        int money = isNumeric(input);
+        isThousandUnit(money);
+        return money;
+    }
+
+    private int isNumeric(String input) {
+        int money;
+        try {
+            money = Integer.parseInt(input);
+        } catch (Exception e){
+            throw new IllegalArgumentException("구매 금액은 숫자로 입력해야 합니다.");
+        }
+        return money;
+    }
+
+    private void isThousandUnit(int money) {
+        if (money % 1000 != 0){
+            throw new IllegalArgumentException("구매 금액은 1000원 단위로 입력해야 합니다.");
+        }
+    }
 }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -1,4 +1,32 @@
 package lotto.view;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.InputAnalysisProcess;
+
 public class InputViewTest {
+
+    @Test
+    void checkThousandUnit() {
+        InputView inputView = new InputView();
+        String input = "10000";
+        Assertions.assertEquals(inputView.receiveMoneyUserInput(input), 10000);
+    }
+    @Test
+    void checkNotThousandUnit() {
+        InputView inputView = new InputView();
+        String input = "12345";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveMoneyUserInput(input);
+        });
+    }
+
+    @Test
+    void checkStringInput() {
+        InputView inputView = new InputView();
+        String input = "abcde";
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveMoneyUserInput(input);
+        });
+    }
 }
