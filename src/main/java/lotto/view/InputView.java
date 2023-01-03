33525b8b0ea17ad0c8ee0958d0d16feb55e5f430
person003333 <person00333@gmail.com>
2023-01-03 12:12:33 +0900

    feat : 보너스 번호 유저 인풋

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -35,6 +35,7 @@ public class InputView {
         lottoNumbers = stringToIntgerList(scanner.nextLine());
         checkSize(lottoNumbers);
         checkDuplicate(lottoNumbers);
+        checkLottoNumbersInRange(lottoNumbers);
 
         return lottoNumbers;
     }
@@ -64,4 +65,33 @@ public class InputView {
             throw new IllegalArgumentException("로또 번호는 중복 입력 해서는 안됩니다.");
         }
     }
+
+    private void checkInRange(int number){
+        if ( number < 1 || number > 45){
+            throw new IllegalArgumentException("로또 번호는 1~45 사이입니다.");
+        }
+    }
+
+    private void checkLottoNumbersInRange(List<Integer> lottoNumbers){
+        for (int lottoNumber: lottoNumbers) {
+            checkInRange(lottoNumber);
+        }
+    }
+
+    public int receiveLastLottoBonusNumber(List<Integer> lottoNumbers) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("보너스 볼을 입력해 주세요.");
+        int bonusNumber = stringToInteger(scanner.nextLine());
+        checkInRange(bonusNumber);
+        checkDuplicateLottoNumberWithBonusNumber(lottoNumbers, bonusNumber);
+        return bonusNumber;
+    }
+
+    private void checkDuplicateLottoNumberWithBonusNumber(List<Integer> lottoNumbers, int bonusNumber){
+        if (lottoNumbers.contains(bonusNumber)){
+            throw new IllegalArgumentException("보너스 볼은 로또 번호와 중복되면 안됩니다.");
+        }
+    }
+
+
 }
--- src/test/java/lotto/view/InputViewTest.java
+++ src/test/java/lotto/view/InputViewTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.InputA
 
 import java.io.ByteArrayInputStream;
 import java.util.Arrays;
+import java.util.List;
 
 public class InputViewTest {
     InputView inputView = new InputView();
+    static final List<Integer> DUMMY_LIST = Arrays.asList(1,2,3,4,5,6);
 
     @Test
     void checkThousandUnit() {
@@ -61,10 +63,53 @@ public class InputViewTest {
 
     @Test
     void checkLastLottoNumbersString(){
-        String input = "1,2,3,4,5,5\n";
+        String input = "1,2,3,4,5,a\n";
         systemIn(input);
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             inputView.receiveLastLottoNumbers();
+        });
+    }
+
+    @Test
+    void checkLastLottoNumbersOutRange(){
+        String input = "51,52,53,54,56,55\n";
+        systemIn(input);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveLastLottoNumbers();
+        });
+    }
+
+    @Test
+    void checkLastLottoBonusNumber(){
+        String input = "7\n";
+        systemIn(input);
+        Assertions.assertEquals(inputView.receiveLastLottoBonusNumber(Arrays.asList(1,2,3,4,5,6)), 7);
+    }
+
+    @Test
+    void checkLastLottoBonusNumberOutRange(){
+        String input = "51\n";
+        systemIn(input);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveLastLottoBonusNumber(DUMMY_LIST);
+        });
+    }
+
+    @Test
+    void checkLastLottoBonusNumberString(){
+        String input = "abc\n";
+        systemIn(input);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveLastLottoBonusNumber(DUMMY_LIST);
+        });
+    }
+
+    @Test
+    void checkLastLottoBonusNumberDuplicateWithLottoNumbers(){
+        String input = "1\n";
+        systemIn(input);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            inputView.receiveLastLottoBonusNumber(DUMMY_LIST);
         });
     }
 
