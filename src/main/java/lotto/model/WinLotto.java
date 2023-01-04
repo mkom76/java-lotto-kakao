4eaf3af3cccfdd543e974d8ec8fdd11d611b0d8c
mkom76 <mkom76@yonsei.ac.kr>
2023-01-04 18:50:02 +0900

    refactor : add WinLotto class

---
+++ src/main/java/lotto/model/WinLotto.java
@@ -0,0 +1,19 @@
+package lotto.model;
+
+public class WinLotto {
+
+    private final Lotto lotto;
+    private final BonusNumber bonusNumber;
+
+    public WinLotto(Lotto lotto, BonusNumber bonusNumber) {
+        checkDuplicate(lotto, bonusNumber);
+        this.lotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void checkDuplicate(Lotto lotto, BonusNumber bonusNumber) {
+        if(lotto.getLotto().contains(bonusNumber.getBonusNumber())){
+            throw new IllegalArgumentException("보너스 숫자는 로또 숫자와 겹칠 수 없습니다.");
+        }
+    }
+}
