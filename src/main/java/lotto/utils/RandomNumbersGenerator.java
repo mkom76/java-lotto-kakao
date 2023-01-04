5dff5bc3b4cb824e59b48902a891be2c19634081
mkom76 <mkom76@yonsei.ac.kr>
2023-01-04 17:07:58 +0900

    refactor : modify logic of making init 1~45 list in generator

--- src/main/java/lotto/utils/RandomNumbersGenerator.java
+++ src/main/java/lotto/utils/RandomNumbersGenerator.java
@@ -1,6 +1,9 @@
 package lotto.utils;
 
+import lotto.constant.Constants;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,17 +13,21 @@ import java.util.List;
  */
 public class RandomNumbersGenerator {
 
+    private final static List<Integer> INIT_LIST =
+            Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+                    41, 42, 43, 44, 45);
+
     private RandomNumbersGenerator() {
 
     }
 
     public static List<Integer> generateRandomNumbers() {
-        List<Integer> numbers = new ArrayList<>();
+        List<Integer> numbers = new ArrayList<>(INIT_LIST);
 
-        for (int i = 1; i <= 45; i++) {
-            numbers.add(i);
-        }
         Collections.shuffle(numbers);
-        return numbers.subList(0, 6);
+        return numbers.subList(0, Constants.LOTTO_SIZE);
     }
 }
